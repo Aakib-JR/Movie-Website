c7aabc7da30630ca087d76ce304f342c4e3f2c6f
Aakib-JR <aakibcr83@gmail.com>
2026-07-10 00:43:43 +0530

    feat(movie-date): implement movie date component

---
+++ src/components/MovieDate.jsx
@@ -0,0 +1,12 @@
+import './movieDate.css'
+
+function MovieDate({movie}) {
+
+    return (
+            <div className={`date ${movie.active ? 'active' : undefined}`}>
+                <h2>{movie.date}</h2>
+            </div>
+    )
+}
+
+export default MovieDate;
