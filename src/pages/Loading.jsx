d04a60a48c21678edfecd5b44709d885b70daf7b
Aakib-JR <aakibcr83@gmail.com>
2026-07-12 20:23:11 +0530

    feat(loading): implement loading page

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,11 @@
+import './loading.css';
+
+function Loading(){
+    return(
+        <div className="loading">
+            <img src="https://media.tenor.com/tga0EoNOH-8AAAAM/loading-load.gif" alt="Loading-Image" />
+        </div>
+    )
+}
+
+export default Loading;
